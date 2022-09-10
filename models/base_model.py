a1e9ceef18d1493bfc6271a2630d381b150553fd
Mulugeta Achiso <anem1351@gmail.com>
2022-09-10 02:37:29 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,66 @@
 #!/usr/bin/python3
-"""Model Base """
+"""
+base class file
+"""
+
 import uuid
-import models
 from datetime import datetime
+import models
+from json import JSONEncoder
 
 
 class BaseModel:
-    """class Base"""
-    def __init__(self, *args, **kwargs):
-        """ Constructor """
-        if kwargs:
-            # self.__dict__ = kwargs
-            # self.created_at = datetime.strptime(self.created_at,
-            #                                     "%Y-%m-%dT%H:%M:%S.%f")
+    """
+    =========
+    BaseModel
+    =========
+    """
 
-            # self.updated_at = datetime.strptime(self.updated_at,
-            #                                     "%Y-%m-%dT%H:%M:%S.%f")
+    def __init__(self, *args, **kwargs):
+        """initialize the instance of the class"""
+        if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at":
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key == "updated_at":
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != "__class__":
-                    setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(
+                            value,
+                            '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "__class__":
+                    continue
+
+                setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())  # unique id
-            self.created_at = datetime.now()  # datetime when is created
-            self.updated_at = datetime.now()  # date when is updated
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
-    def __str__(self):
-        """ print() __str__ method """
-        """" For pep8 validation"""
-        className = self.__class__.__name__
-        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
-
     def save(self):
-        """ updates with the current datetime """
+        """save new informations to the class object"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        '''returns a dictionary with all keys/value of the instance'''
-        dict_copy = self.__dict__.copy()
-        dict_copy["created_at"] = self.created_at.isoformat()
-        dict_copy["updated_at"] = self.updated_at.isoformat()
-        dict_copy['__class__'] = self.__class__.__name__
-        return dict_copy
+        """return dictionary representaton of the instance"""
+        dict_repr = {}
+        for key, value in self.__dict__.items():
+            dict_repr[key] = value
+            if isinstance(value, datetime):
+                dict_repr[key] = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        dict_repr["__class__"] = type(self).__name__
+        return dict_repr
+
+    def __str__(self):
+        """return the string formated message when instance is called"""
+        clName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clName, self.id, self.__dict__)
+
+
+class BaseModelEncoder(JSONEncoder):
+    """JSON Encoder for BaseModel
+    """
+
+    def default(self, o):
+        """ default"""
+        if isinstance(o, BaseModel):
+            return o.to_dict()
+        return super().default(o)
